b8a7cb367786d1510d4e54f2e27ce25ae0a42986
gbarska <gbarska@yahoo.com>
2023-02-23 10:24:51 -0300

    First commit

---
+++ src/lib/index.js
@@ -0,0 +1,5 @@
+// Reexport your entry components here
+export { default as Toggle } from "./Toggle.svelte";
+export { default as MyButton } from "./MyButton.svelte";
+export { default as MyText } from "./MyText.svelte";
+
